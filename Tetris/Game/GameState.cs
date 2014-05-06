39c93f050a05b4206da9674e30127c1f771ec1a8
Костадин Поп-Кочев <kostadin.popkocev@gmail.com>
2014-05-06 15:28:39 +0200

    Fix

---
+++ Tetris/Game/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    public interface GameState
+    {
+        void moveDown();
+        void moveLeft();
+        void moveRight();
+        void rotate();
+        void draw(Graphics g);
+    }
+}
---
+++ Tetris/Game/PausedState.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    class PausedState : GameState
+    {
+        private Game game;
+
+        public PausedState(Game g)
+        {
+            game = g;
+        }
+        public void moveDown()
+        {
+        }
+
+        public void moveLeft()
+        {
+        }
+
+        public void moveRight()
+        {
+        }
+
+        public void draw(System.Drawing.Graphics g)
+        {
+            try
+            {
+                Image img = Properties.Resources.PauseImage;    
+                foreach (TetrisForm t in game.tetrisForms)
+                {
+                    t.draw(g);
+                }
+                g.DrawImage(img, new Point(25, 100));
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Poraka: " +e.Message);
+            }
+        }
+
+
+        public void rotate()
+        {
+
+        }
+    }
+}
